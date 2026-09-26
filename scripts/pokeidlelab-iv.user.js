62788d7abf684600ed7e8e150434c23a7e64841f
Tito <titooficial92@gmail.com>
2026-09-26 19:29:13 +0200

    Bump Analyzer to v1.0.32

--- scripts/pokeidlelab-iv.user.js
+++ scripts/pokeidlelab-iv.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         PokeIdleLab Calculator
 // @namespace    poke-idle-lab
-// @version      1.0.31
+// @version      1.0.32
 // @description  Calculadora de IV para Poke Idle World, integrada con PokeGrid
 // @match        https://poke.idleworld.online/*
 // @grant        none
