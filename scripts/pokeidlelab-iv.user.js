5df70df3ca3135d17202b68e90292d41f08aa837
GigaBuda <titooficial92@gmail.com>
2026-09-26 12:25:40 +0200

    Update PokeIdleLab UI and move ordering

--- scripts/pokeidlelab-iv.user.js
+++ scripts/pokeidlelab-iv.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         PokeIdleLab Calculator
 // @namespace    poke-idle-lab
-// @version      1.0.8
+// @version      1.0.9
 // @description  Calculadora de IV para Poke Idle World, integrada con PokeGrid
 // @match        https://poke.idleworld.online/*
 // @grant        none
